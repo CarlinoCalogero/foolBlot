a6554e4f43f5c3a18d985f529b90ee5c90fba558
CarlinoCalogero <calogerocarlino19@gmail.com>
2024-04-09 20:07:48 +0200

    removed mik function, added sad/sadge functions and 'ci pieghiamo' function

--- src/index.ts
+++ src/index.ts
@@ -12,7 +12,7 @@ import { ChatMemberAdministrator } from "./types/ChatMemberAdministrator";
 import { MessageEntity } from "./types/MessageEntity";
 import { Update } from "./types/Update";
 import { User } from "./types/User";
-import { MIK, MI_PIEGO, WOOF, YOOOOOOOOOOO } from "./utils";
+import { MI_PIEGO, MIAO, STICKERS, WOOF, YOOOOOOOOOOO } from "./utils";
 
 export interface Env {
 	TELEGRAM_AUTH_TOKEN: string
@@ -39,14 +39,15 @@ async function processUpdate(request: Request, telegramAuthToken: string) {
 		//console.log(message)
 		if ("text" in message) {
 			const userText = message.text;
-			if (userText.startsWith("/all")) {
+			const lowerCaseUserText = userText.toLowerCase()
+			if (lowerCaseUserText.startsWith("/all")) {
 				const response = await fetch(`https://api.telegram.org/bot${telegramAuthToken}/getChatAdministrators?chat_id=${update.message.chat.id}`);
 				const responseBody: {
 					"ok": boolean,
 					"result": ChatMemberAdministrator[]
 				} = await response.json();
 				const chatMemberAdministrators: ChatMemberAdministrator[] = responseBody.result;
-				
+
 				const messageEntities: MessageEntity[] = [];
 
 				//console.log("bau", chatMemberAdministrators.length, JSON.parse(JSON.stringify(chatMemberAdministrators)))
@@ -54,7 +55,7 @@ async function processUpdate(request: Request, telegramAuthToken: string) {
 				let offset: number = 0;
 				let message: string = ''
 
-				for(let count = 0; count < chatMemberAdministrators.length; count++){
+				for (let count = 0; count < chatMemberAdministrators.length; count++) {
 					const currentChatMember = chatMemberAdministrators[count]
 					const currentUser: User = currentChatMember.user;
 
@@ -67,9 +68,9 @@ async function processUpdate(request: Request, telegramAuthToken: string) {
 							offset: offset,
 							length: mention.length,
 						})
-						if(offset == 0){
+						if (offset == 0) {
 							message = mention
-						} else{
+						} else {
 							message = `${message} ${mention}`
 						}
 						offset = mention.length + 1; //+1 is the space
@@ -80,21 +81,37 @@ async function processUpdate(request: Request, telegramAuthToken: string) {
 				await replyToMessage(telegramAuthToken, update.message, message, messageEntities)
 
 			}
-			if (userText.toLowerCase().includes("mi piego")) {
+			// g == global search, i == case-insenstitive search
+			const miPiegoRegex: RegExp = /mi piego|mi sono piegato|mi fa piegare|mi fa piega|mi ha fatto piegare|mi ha fatto piega|mi piegai|mi piegher(o|ò)/gmi
+			if (miPiegoRegex.test(lowerCaseUserText)) {
 				const senderId = message.from.id
 				await replyToMessage(telegramAuthToken, update.message, MI_PIEGO[senderId])
 			}
+			const ciPieghiamoRegex: RegExp = /ci pieghiamo|ci siamo piegati|ci fa piegare|ci fa piega|ci ha fatto piegare|ci ha fatto piega|ci piegammo|ci piegheremo/gmi
+			if (ciPieghiamoRegex.test(lowerCaseUserText)) {
+				let text = ''
+				for (const [key, value] of Object.entries(MI_PIEGO)) {
+					text = `${text}\n\n${value}`
+				}
+				await replyToMessage(telegramAuthToken, update.message, text)
+			}
 			const yoooRegex: RegExp = /yoo+|yo\s|yo$/gmi
-			if (yoooRegex.test(userText)) {
+			if (yoooRegex.test(lowerCaseUserText)) {
 				await replyToMessage(telegramAuthToken, update.message, YOOOOOOOOOOO.catchPhrase, "", YOOOOOOOOOOO.link)
 			}
-			// g == global search, i == case-insenstitive search
-			const mikRegex: RegExp = /mik/gmi
-			if (mikRegex.test(userText)) {
-				await sendSticker(telegramAuthToken, message)
+			const miaoRegex: RegExp = /miao|nya+|nya\s|nya$/gmi
+			if (miaoRegex.test(lowerCaseUserText)) {
+				await replyToMessage(telegramAuthToken, update.message, MIAO.catchPhrase, "", MIAO.link)
+			}
+			if (lowerCaseUserText.includes("sad") || lowerCaseUserText.includes("sadge")) {
+				await sendSticker(telegramAuthToken, message, STICKERS.kana_1)
+				await sendSticker(telegramAuthToken, message, STICKERS.kana_2)
+			}
+			if (lowerCaseUserText.includes("bruh")) {
+				await sendSticker(telegramAuthToken, message, STICKERS.violin)
 			}
 			const woofRegex: RegExp = /wo+f|grr+|bark|snarl|arf|bark|awo+/gmi
-			if (woofRegex.test(userText)) {
+			if (woofRegex.test(lowerCaseUserText)) {
 				await replyToMessage(telegramAuthToken, update.message, WOOF)
 			}
 		}
@@ -113,12 +130,12 @@ async function replyToMessage(telegramAuthToken: string, message: any, responseT
 	await fetch(url);
 }
 
-async function sendSticker(telegramAuthToken: string, message: any) {
+async function sendSticker(telegramAuthToken: string, message: any, stickerFileId: string) {
 	const chatId = message.chat.id;
 	const replyParameters = {
 		message_id: message.message_id
 	}
-	const url = `https://api.telegram.org/bot${telegramAuthToken}/sendSticker?chat_id=${chatId}&sticker=${MIK.stickerFileId}&reply_parameters=${JSON.stringify(replyParameters)}`;
+	const url = `https://api.telegram.org/bot${telegramAuthToken}/sendSticker?chat_id=${chatId}&sticker=${stickerFileId}&reply_parameters=${JSON.stringify(replyParameters)}`;
 	await fetch(url);
 }
 
--- src/utils.ts
+++ src/utils.ts
@@ -4,7 +4,9 @@ export const MI_PIEGO: Record<number, string> = {
     5663655397: "\\_\n    uca",
     232414549: "Raffae\\\n             e",
     2135558691: "Manue\\\n                 Di Cresce",
-    279499618: "Giacomo Pao\\\n                             occi"
+    279499618: "Giacomo Pao\\\n                             occi",
+    150074880: "Enrico Meniche\\\n                                 \\i",
+    313592737: "Michae\\\n                 Piccirilli"
 }
 
 export const YOOOOOOOOOOO = {
@@ -12,8 +14,15 @@ export const YOOOOOOOOOOO = {
     link: "https://www.youtube.com/watch?v=VKMw2it8dQY"
 }
 
-export const MIK = {
-    stickerFileId: "CAACAgEAAxkBAAIBOWXyKNjH1v1tl6ETR3nP2kC8QhCLAAI5AgACS-SRRKjgVUzXGD6ENAQ"
+export const MIAO = {
+    catchPhrase: ".",
+    link: "https://www.youtube.com/watch?v=4GQuHsKUQk8"
 }
 
 export const WOOF = "GRRRRRRRRRRR WOOF WOOF BARK BARK ARF BARK GRRRR WOOF SNARL HSSSS GRRRR WOOF WOOF BARK ARF GRRRR HSSSS WOOF WOOF BARK ARF GRRRRR HSSSSS BARK ARF GRRRR WOOF WOOF BARK GRRR SNARL ARF WOOF BARK ARF SNARL HSSSSSS AWOO0000\nAWOO0000000 WOOOOOOF WOOOOOF"
+
+export const STICKERS = {
+    violin: "CAACAgEAAxkBAAIBOWXyKNjH1v1tl6ETR3nP2kC8QhCLAAI5AgACS-SRRKjgVUzXGD6ENAQ",
+    kana_1: "CAACAgUAAxkBAAIBnWX9WVFwGXJPl7z_BnpEGqgVrsdWAAICCgACg2eIV2MAAQTk3__YxTQE",
+    kana_2: "CAACAgQAAxkBAAIBnmX9WVGbDFoZWJ1MRh_i5w1aNSA4AAKsEwAChjtRU3fJ-49cnmGZNAQ"
+}
